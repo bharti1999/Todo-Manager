eb0db452fa00604aeee1bf63b9a4c49a96e85d01
Bharti Sharma <bharti.sharma@nagarro.com>
2020-04-17 01:34:17 +0530

    remved port as env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const todoRoute = require('./routes/todos');
 const sort = require('./routes/sort');
 const notes =require('./routes/notes')
 const app = express();
-const port=process.env.PORT||3000
+//const port=process.env.PORT||3000
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
@@ -18,7 +18,7 @@ app.use('/todos',notes);
 
 db.sync()
     .then( () => {
-        app.listen(port);
+        app.listen(3000);
     })
     .catch((err) => {
         console.error(err);
